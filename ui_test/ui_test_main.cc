1bc2d7ae974b215347cbe1cac0fe66618efbddf2
zheng.che <zheng.che.hz@gmail.com>
2015-02-13 01:27:09 +0800

    Add the project ui_test.

---
+++ ui_test/ui_test_main.cc
@@ -0,0 +1,8 @@
+﻿#include <windows.h>
+
+int __stdcall wWinMain(HINSTANCE instance_handle, 
+                       HINSTANCE prev_instance_handle,
+                       wchar_t* command_line,
+                       int unused) {
+  MessageBox(nullptr, L"Hello Windows!", L"Talk to me", MB_OK);
+}
